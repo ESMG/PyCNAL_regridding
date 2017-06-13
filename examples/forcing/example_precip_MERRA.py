cd87b3d3505becfa340b5e967165b056eeb7a888
Raphael Dussin <raphael.dussin@gmail.com>
2017-06-13 17:10:56 -0400

    RD: improved example

--- examples/forcing/example_precip_MERRA.py
+++ examples/forcing/example_precip_MERRA.py
@@ -8,7 +8,7 @@ eraigrd = direxamples + 'lsm_erainterim_roms_conserve.nc'
 # ---------- define segments on ERAint grid -----------------------
 domain = obc_segment('domain', eraigrd,target_model='regular',istart=0,iend=511,jstart=0,  jend=255)
 
-# weirdness, interp from merra to merra does not work...
+# as a test we can do MERRA to MERRA too
 #domain = obc_segment('domain', merrafile,target_model='regular',istart=0,iend=539,jstart=0,  jend=360)
 
 # ---------- define variables on each segment ------------------
@@ -20,11 +20,11 @@ list_variables = [rain_domain]
 list_vectvariables = []
 
 # ---------- bilinear interpolation from MERRA file -------------------
-interp_t2s_bilin = rain_domain.interpolate_from(merrafile,'rain',timename='rain_time',frame=0,coord_names=['lon','lat'],method='bilinear',drown='no')
+interp_t2s_bilin = rain_domain.interpolate_from(merrafile,'rain',timename='rain_time',frame=0,coord_names=['lon','lat'],method='bilinear',drown='no',autocrop=False)
 lib_ioncdf.write_obc_file(list_segments,list_variables,list_vectvariables,rain_domain.timesrc,output='merra_rain_bilin_fr0000.nc')
 
 for kt in np.arange(1,16):
-	rain_domain.interpolate_from(merrafile,'rain',timename='rain_time',frame=kt,coord_names=['lon','lat'],method='bilinear',drown='no',interpolator=interp_t2s_bilin)
+	rain_domain.interpolate_from(merrafile,'rain',timename='rain_time',frame=kt,coord_names=['lon','lat'],method='bilinear',drown='no',interpolator=interp_t2s_bilin,autocrop=False)
 	lib_ioncdf.write_obc_file(list_segments,list_variables,list_vectvariables,rain_domain.timesrc,output='merra_rain_bilin_fr' + str(kt).zfill(4) + '.nc')
 
 # ---------- conservative interpolation from MERRA file -------------------
@@ -32,5 +32,5 @@ interp_t2s_conserve = rain_domain.interpolate_from(merrafile,'rain',timename='ra
 lib_ioncdf.write_obc_file(list_segments,list_variables,list_vectvariables,rain_domain.timesrc,output='merra_rain_conserve_fr0000.nc')
 
 for kt in np.arange(1,16):
-	rain_domain.interpolate_from(merrafile,'rain',timename='rain_time',frame=kt,coord_names=['lon','lat'],method='conserve',drown='no',interpolator=interp_t2s_conserve)
-	lib_ioncdf.write_obc_file(list_segments,list_variables,list_vectvariables,rain_domain.timesrc,output='merra_rain_conserve_ff' + str(kt).zfill(4) + '.nc')
+	rain_domain.interpolate_from(merrafile,'rain',timename='rain_time',frame=kt,coord_names=['lon','lat'],method='conserve',drown='no',interpolator=interp_t2s_conserve,use_gridspec=True)
+	lib_ioncdf.write_obc_file(list_segments,list_variables,list_vectvariables,rain_domain.timesrc,output='merra_rain_conserve_fr' + str(kt).zfill(4) + '.nc')
